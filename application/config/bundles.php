f0dc83e37d87c52b0328788619caab1a69dc2f50
Marcin Pyrka <marcin.pyrka@polcode.net>
2018-07-27 17:31:09 +0200

    Update

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
