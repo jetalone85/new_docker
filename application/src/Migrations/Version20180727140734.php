55c50ead8fc5973d5e7cfecf5204fc11c28fc422
Marcin Pyrka <marcin.pyrka@polcode.net>
2018-07-27 16:20:26 +0200

    Edit migration

--- application/src/Migrations/Version20180727140734.php
+++ application/src/Migrations/Version20180727140734.php
@@ -15,10 +15,8 @@ final class Version20180727140734 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, login VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, salt VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('DROP TABLE book');
     }
 
     public function down(Schema $schema) : void
@@ -28,8 +26,6 @@ final class Version20180727140734 extends AbstractMigration
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE book (id INT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('DROP TABLE "user"');
     }
 }
