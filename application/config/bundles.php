47b55f32dad1643575ba9ed42679743402d1163e
ubuntu <ubuntu@ambari.ubuntu>
2018-07-27 21:40:16 +0200

    Update

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -13,4 +13,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Bazinga\Bundle\FakerBundle\BazingaFakerBundle::class => ['all' => true],
 ];
